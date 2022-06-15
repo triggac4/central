0b59ab5ce23c832bca6feb7e176b4d39d88a4bf6
solomon-prosperity <prosper@youverify.co>
2022-06-15 17:18:12 +0100

    project structure setup

--- test/index.test.js
+++ test/index.test.js
@@ -8,7 +8,7 @@ const { expect, request } = chai;
 const port = config.port;
 const host = `127.0.0.1:${port}`;
 
-describe('API TESTS ', () => {
+describe('CAMPUS CETRAL API TESTS ', () => {
   //The local server needs to be running for this test to pass
   it('base url should return a status code of 200', async () => {
     const res = await request(host)
