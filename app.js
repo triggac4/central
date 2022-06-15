05f54963667fac120f1ac25e6ec0cca7160c4fec
solomon-prosperity <prosper@youverify.co>
2022-06-15 16:38:55 +0100

    project structure setup

--- app.js
+++ app.js
@@ -1,10 +1,54 @@
 import express from 'express';
 import config from './config/default.js';
 import logger from './src/utils/logger.js';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cors from 'cors';
+import { connectDB } from './src/database/mongoose.js';
 
 const app = express();
 const port = config.port;
 
+// setup helmet
+// Helmet includes a whopping 12 packages that all work to block malicious parties
+// from breaking or using an application to hurt its users
+app.use(helmet());
+
+//Body Parser
+app.use(express.json());
+
+// log only 4xx and 5xx responses to console
+const logFormat =
+  process.env.NODE_ENV == 'production' ? 'combined' : 'development';
+app.use(
+  morgan(logFormat, {
+    skip(req, res) {
+      return res.statusCode < 500;
+    },
+  })
+);
+
+// Enable CORS
+app.use(
+  cors({
+    origin: (origin, cb) => {
+      const allowed_origins = config.allowed_origins;
+      if (allowed_origins.trim() == '*') {
+        cb(null, true);
+      } else {
+        const origins = allowed_origins.split(',');
+        if (origins.indexOf(origin) != -1 || !origin) {
+          cb(null, true);
+        } else {
+          cb(new Error(`Origin('${origin}') not allowed`, false));
+        }
+      }
+    },
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  })
+);
+
+//Base URL
 app.get('/', (req, res) => {
   res.status(200).json({
     message: 'API v1  is running',
@@ -13,6 +57,7 @@ app.get('/', (req, res) => {
   });
 });
 
-app.listen(port, () => {
-  logger.info(`server is listening on ${port}`);
+app.listen(port, async () => {
+  await connectDB();
+  logger.info(`server is listening on port ${port}`);
 });
---
+++ src/database/mongoose.js
@@ -0,0 +1,68 @@
+import mongoose from 'mongoose';
+import logger from '../utils/logger.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+// required environment variables
+// ["NODE_ENV", "PORT"].forEach((name) => {
+//     if (!process.env[name]) {
+//       logger.error(`Environment variable ${name} is missing`);
+//       process.exit(1);
+//     }
+//   });
+
+export const connectDB = (poolSize = 20, autoIndex = true) => {
+  let dbName;
+  let connectionString;
+  switch (process.env.NODE_ENV) {
+    case 'production':
+      dbName = process.env.MONGO_DATABASE;
+      connectionString = `mongodb+srv://${encodeURIComponent(
+        process.env.MONGO_USER
+      )}:${encodeURIComponent(process.env.MONGO_PASS)}@${
+        process.env.MONGO_HOST
+      }/${encodeURIComponent(
+        process.env.MONGO_DATABASE
+      )}?retryWrites=true&w=majority`;
+      break;
+    case 'staging':
+      dbName = process.env.MONGO_DATABASE;
+      connectionString = `mongodb+srv://${encodeURIComponent(
+        process.env.MONGO_USER
+      )}:${encodeURIComponent(process.env.MONGO_PASS)}@${
+        process.env.MONGO_HOST
+      }/campus_central_staging?retryWrites=true&w=majority`;
+      break;
+    default:
+      dbName = process.env.MONGO_DATABASE;
+      mongoose.set('debug', true);
+      connectionString = 'mongodb://localhost:27017';
+  }
+
+  const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    autoIndex,
+    dbName,
+  };
+
+  const db = mongoose.connect(connectionString, options).catch((err) => {
+    if (err.message.indexOf('ECONNREFUSED') !== -1) {
+      logger.error(
+        "Error: The server was not able to reach MongoDB. Maybe it's not running?"
+      );
+      process.exit(1);
+    } else {
+      throw err;
+    }
+  });
+  db.then(() => {
+    logger.info('Successfully connected to MongoDB');
+  }).catch((error) => {
+    logger.error('An error occurred while trying to connect to MongoDB', {
+      error: error.toString(),
+    });
+  });
+
+  return db;
+};
